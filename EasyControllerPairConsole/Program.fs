28d905f71c21749e10dd15c76ef68669296cf86e
Udi <udiberes@gmail.com>
2021-10-25 09:02:46 +0300

    WIP

--- EasyControllerPairConsole/Program.fs
+++ EasyControllerPairConsole/Program.fs
@@ -1,4 +1,21 @@
+open System
+
+//let temp =
+//    while true do
+//        printfn "please select a key"
+//        let key = Console.ReadKey()
+//        printfn $"chosen key: %s{key.Key.ToString()}"
+
+
 [<EntryPoint>]
-let main argv =
-    printfn "Started"
-    0 // return an integer exit code
+let main _ =
+    let doStuff key =
+        printfn $"\nDoing stuff with key: %c{key}"
+
+    let rec checkForKeys (key: char) =
+        if key = 'w' then
+            doStuff key
+            checkForKeys (Console.ReadKey().KeyChar)
+
+    checkForKeys (Console.ReadKey().KeyChar)
+    0
